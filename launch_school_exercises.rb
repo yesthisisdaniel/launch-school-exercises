c7f95d776464859f08c67ad9331ec6d56413ecdd
Daniel Nowak <33090108+yesthisisdaniel@users.noreply.github.com>
2026-08-04 07:55:12 +0000

    Add make attribute to MyCar class

--- launch_school_exercises.rb
+++ launch_school_exercises.rb
@@ -1,22 +1,23 @@
 # Your code goes here
 class MyCar
-  attr_reader :year, :color, :model
+  attr_reader :year, :color, :model, :make
   attr_accessor :speed 
 
   def self.gas_mileage(miles, gallon)
     miles / gallon
   end
   
-  def initialize(year, color, model)
+  def initialize(year, color, make, model)
     @year = year
     @color = color
     @model = model
+    @make = make
     @speed = 0
     @engine_status = "off"
   end
 
   def to_s 
-    "This is a ${year}, ${color} colored (name of object) ${model}"
+    "This is a ${@year}, ${@color} colored ${@make} ${@model}"
   end
   
   def ignition(key)
@@ -47,3 +48,4 @@ class MyCar
       @speed = 0
     end
   end
+end
