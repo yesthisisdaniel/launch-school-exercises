c069d665f080cc262e2eb676deb1969c838410f5
Daniel Nowak <33090108+yesthisisdaniel@users.noreply.github.com>
2026-08-06 13:27:14 +0000

    Refactor MyCar class into Vehicle superclass

--- launch_school_exercises.rb
+++ launch_school_exercises.rb
@@ -1,29 +1,15 @@
 # Your code goes here
-class MyCar
-  attr_reader :year, :color, :model, :make
-  attr_accessor :speed 
 
-  def self.gas_mileage(miles, gallon)
+class Vehicle
+   def self.gas_mileage(miles, gallon)
     miles / gallon
   end
-  
-  def initialize(year, color, make, model)
-    @year = year
-    @color = color
-    @model = model
-    @make = make
-    @speed = 0
-    @engine_status = "off"
-  end
 
-  def to_s 
-    "This is a #{year}, #{color} colored #{make} #{model}"
-  end
-  
   def ignition(key)
     if key == "in"
       @engine_status = "on"
     end
+  end
   
   def accelerate(kmh)
     if @engine_status == "off"
@@ -39,12 +25,62 @@ class MyCar
     else
       @speed = @speed - kmh
     end
-    end
+  end
   
   def shut_down(key)
     if key == "out"
       @engine_status = "off"
       @speed = 0
     end
+  end
+end
+
+class MyCar < Vehicle
+  attr_reader :year, :color, :model, :make
+  attr_accessor :speed 
+
+  # def self.gas_mileage(miles, gallon)
+  #   miles / gallon
+  # end
+  
+  def initialize(year, color, make, model)
+    @year = year
+    @color = color
+    @model = model
+    @make = make
+    @speed = 0
+    @engine_status = "off"
+  end
+
+  def to_s 
+    "This is a #{year}, #{color} colored #{make} #{model}"
+  end
+  
+  # def ignition(key)
+  #   if key == "in"
+  #     @engine_status = "on"
+  #   end
+  
+  # def accelerate(kmh)
+  #   if @engine_status == "off"
+  #     puts "Please turn car on"
+  #   else
+  #     @speed += kmh
+  #     end
+  #   end
+ 
+  # def brake(kmh)
+  #   if @speed <= 0
+  #     return
+  #   else
+  #     @speed = @speed - kmh
+  #   end
+  #   end
+  
+  # def shut_down(key)
+  #   if key == "out"
+  #     @engine_status = "off"
+  #     @speed = 0
+  #   end
   end
 end
